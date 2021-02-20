5f177684b8c8359bb1013ceaeb30054eca1acba6
Dave Lame <baronniall@aol.com>
2021-02-20 02:59:34 -0500

    Another ball tracker

--- 2021Experiments/Onesampleballfinder.py
+++ 2021Experiments/Onesampleballfinder.py
@@ -3,13 +3,11 @@ import numpy as np
 import math
 import time
 import sys
-import threading
-from networktables import NetworkTables
 
 
-imageCounter=0
+imageCounter=10
 maxSeconds=120
-delaySeconds=30
+delaySeconds=10
 
 imageHeight=480
 imageWidth=640
@@ -23,65 +21,46 @@ erosionKernel=np.ones((9,9),np.uint8)
 
 
 #yellow
-lowColor=np.array([15,100,190])
-highColor=np.array([40,200,255])
+lowColor=np.array([15,80,130])
+highColor=np.array([40,255,255])
 
-robotAddress="10.2.47.2"  #The address of the network tables server.  Usually the robot
 
-cond=threading.Condition()
-notified = [False]
+firstTime=True    
 
-def connectionListener(connected,info):
-    print(info,'; Connected=%s' % connected)
-    with cond:
-        notified[0] = True
-        cond.notify()
-
-NetworkTables.initialize(server="10.2.47.2")
-NetworkTables.addConnectionListener(connectionListener,immediateNotify=True)
-
-with cond:
-    print("Waiting")
-    if not notified[0]:
-        cond.wait()
-
-sd=NetworkTables.getTable('SmartDashboard')
-sd.putNumber("Sample Number",247)
-
-startTime=time.time()
-now=time.time()
-
-while(now-startTime<delaySeconds):
-    now=time.time()
-    
-sd.putBoolean("Capturing Photos",True)
-cap = cv2.VideoCapture(0)
-if cap.isOpened():
+if True:
     startCapture=time.time()
-    while(now-startCapture<maxSeconds):
+    now=time.time()
+    while(firstTime):
+        firstTime=False;
         imageCounter+=1
         print(str(imageCounter))
-        ret, inimg = cap.read()
         savefilename="/home/pi/junk/saveimage"+str(imageCounter)+".png"
-        processedfilename="/home/pi/junk/processedimage"+str(imageCounter)+".png"
-        maskname="/home/pi/junk/mask"+str(imageCounter)+".png"
+        processedfilename="/home/pi/junk/processedimagej"+str(imageCounter)+".png"
+        maskname="/home/pi/junk/maskj"+str(imageCounter)+".png"
+        inimg=cv2.imread(savefilename)
 
         flipped=cv2.flip(inimg,0) # flip about the x axis
-        cv2.imwrite(savefilename,flipped)
+        #cv2.imwrite(savefilename,flipped)
+        cv2.imshow('in',flipped)
         
         #trim the image.
         
         #cropped=flipped[imageHeight/2:imageHeight-1,0:imageWidth-1]
         cropped=flipped
+        cv2.imshow('cropped',cropped)
+        
         
         #try it without blurring first
         #blurred=cv2.medianBlur(inimg,7)
         imghls = cv2.cvtColor(cropped, cv2.COLOR_BGR2HLS)
+        cv2.imshow('imghls',imghls)
         mask=cv2.inRange(imghls,lowColor,highColor)
         erosion=cv2.erode(mask,erosionKernel,iterations = 1)
         mask=cv2.dilate(erosion,erosionKernel,iterations = 1)
+        cv2.imshow('mask',mask)
         cv2.imwrite(maskname,mask)
         
+
 
         # Find contours
         contours,hierarchy=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -95,30 +74,32 @@ if cap.isOpened():
         # High numbers of points
         biggestContourArea=0
         biggestContour=None
-        ballindex=-1
         index=-1
-        print("cont "+str(len(contours)))
+        ballindex=-1
         if (len(contours)>0):
             for c in contours:
                 index+=1
                 if (len(c)<MIN_VERTEX_COUNT):
                     continue
                 cArea=cv2.contourArea(c)
-                if (cArea<MIN_AREA) or(cArea<=biggestContourArea):
+                if (cArea<MIN_AREA) or (cArea<=biggestContourArea):
                     continue
                 x,y,w,h=cv2.boundingRect(c)
                 ratio=(float)(w)/h
                 minRatio=(float)(1)/MAX_RATIO
                 if (ratio<minRatio) or (ratio>MAX_RATIO):
                     continue;
-                #if you get to here, you are golden
+                #if you get to here, you are g olden
                 biggestContourArea=cArea
                 biggestContour=c
                 ballfound=True
-                xcenter=x+w/2-imageWidth/2
-                ballTheta=cameraFOV*xcenter/(imageWidth/2)#should be arctan x/F,
-                ballindex=index
                 
+                xcenter=x+w/2-imageWidth/2
+                ballTheta=cameraFOV*xcenter/(imageWidth/2)
+                ballindex=index
+                print('x= '+str(x))
+                print(xcenter)
+                print(ballTheta)
                 
                 
                 
@@ -129,19 +110,20 @@ if cap.isOpened():
         #write the angle to the smartdashboard
         #also write boolean "found" as true or false, depending on whether any contour matched
 
-        sd.putBoolean("Ball Found",ballfound)
-        sd.putNumber("Ball Yaw",ballTheta)
-        
+        #sd.putBoolean("Ball Found",ballfound)
+        #sd.putNumber("Ball Yaw",ballTheta)
         if (ballindex!=-1):
             cv2.drawContours(cropped,contours,ballindex,(0,255,0),3)
-            print("Angle "+str(ballTheta))
         cv2.imwrite(processedfilename,cropped)
-        now=time.time()
-    
-sd.putBoolean("Capturing Photos",False)
-cap.release()
+        cv2.imshow('ball',cropped)
+        while(now-startCapture<maxSeconds):
+             now=time.time()
+             time.sleep(1)
+        
+#cap.release()
 cv2.destroyAllWindows()
 sys.exit()
+
 
 
 
